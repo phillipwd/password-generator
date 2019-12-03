0ecbb456413c4d2a6615d437fd767982592018c3
Phillip Dunham <phillipwwd@gmail.com>
2019-12-03 15:24:34 -0800

    Handling inputs

--- script.js
+++ script.js
@@ -7,31 +7,47 @@ var copyBtn = document.querySelector("#copy");
 //this function will fire when you click the generate password button on the page.  I've set it to alert "You've clicked a button" and return a password of password for now. Update it to make your password
 function generatePassword() {
     //YOUR CODE HERE
-    var nums = confirm("Would you like to use numbers?");
-    var sp = confirm("Would you like to use special characters?");
-    var low = confirm("Would you like to use lower case letters?");
-    var up = confirm("Would you like to use upper case letters?");
+    var nums;
+    var sp;
+    var low;
+    var up;
+    var options = 0;
+    while(options===0){
+        nums = confirm("Would you like to use numbers?");
+        sp = confirm("Would you like to use special characters?");
+        low = confirm("Would you like to use lower case letters?");
+        up = confirm("Would you like to use upper case letters?");
+        options = nums+sp+low+up;
+        if(options===0){
+            alert("Please select character type(s)");
+        }
+    }
     var len = prompt("How many characters would you like? Choose 8-128");
-    var numbers = [1,2,3,4,5,6,7,8,9] //is numbers
+    var numbers = [1,2,3,4,5,6,7,8,9]; //is numbers
     var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];//is letters    
-    var special = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]//is characters
+    var special = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];//is characters
 
     var poss = [];
-    
-    // for (i = 0; i < inputsValue[4]; i++){
-    //     //works from
-    //     var types = Math.floor(Math.random()*inputsValue.length); // picks number from short array
 
-        // console.log(typeArray.length);
-        
-        // console.log("types is " + types);// shows number
-        // console.log(typeArray[types]); // shows variable name
-        // to
+    if(nums){
+        poss = poss + numbers;       
+        // console.log(poss[0]);
     }
+
+    if(sp){
+        poss = poss + special;
+        // console.log(poss[0]);
+    }
+
+    if(low){
+        poss = poss + letters;
+        // console.log(poss[0]);
+    }
+    
   
     alert("ive been clicked");
     // return "password";
-// }
+}
 
 // Write password to the #password input
 function writePassword() {
